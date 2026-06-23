4f11c35cf030e2f6dde26417358cb2bf9b807d32
zainabmostajeran900 <zeinabmostajeran586@gmail.com>
2026-06-23 22:44:46 +0330

    fix async login page

--- app/(L&S)/login/page.tsx
+++ app/(L&S)/login/page.tsx
@@ -2,7 +2,7 @@
 import { login } from "@/apis/server-side-apis/auth_service";
 import { LoginSignupForm } from "@/components/L&S/LoginSignupForm";
 
-const Login : React.FC = ()=>{
+const Login = async () => {
     return (
         <div className="grid w-full max-w-screen-sm mx-auto px-2 sm:px-5 mt-10 bg-slate-800 py-10 rounded-md ">
           <h2 className="text-xl sm:text-2xl font-semibold text-center">login to your account</h2>
